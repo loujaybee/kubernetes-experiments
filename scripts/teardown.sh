fd4a0fa0eb34bba9f024716a227cc5fde723278b
Louis Bichard <louis@gitpod.io>
2024-06-17 07:13:58 +0000

    fix: updated teardown

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -5,8 +5,15 @@ export AWS_REGION=us-east-1
 kubectl delete services --all
 kubectl delete deployments --all
 
-aws ecr list-images --repository-name microservice-a --query 'imageIds[*]' --output text | while read imageId; do aws ecr batch-delete-image --repository-name microservice-a --image-ids imageDigest=$imageId; done
-aws ecr list-images --repository-name microservice-b --query 'imageIds[*]' --output text | while read imageId; do aws ecr batch-delete-image --repository-name microservice-b --image-ids imageDigest=$imageId; done
+aws ecr batch-delete-image --region us-east-1 \
+    --repository-name microservice-a \
+    --image-ids "$(aws ecr list-images --region us-east-1 --repository-name microservice-a --query 'imageIds[*]' --output json
+)" || true
+
+aws ecr batch-delete-image --region us-east-1 \
+    --repository-name microservice-b \
+    --image-ids "$(aws ecr list-images --region us-east-1 --repository-name microservice-b --query 'imageIds[*]' --output json
+)" || true
 
 aws ecr delete-repository --repository-name microservice-a --region us-east-1
 aws ecr delete-repository --repository-name microservice-b --region us-east-1
